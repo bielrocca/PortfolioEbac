743cc5d39b78439156c78d7d1b655ebe99cd6985
Gabriel Lucas <bielfefi@outlook.com>
2024-09-11 16:57:32 -0300

    Configurando o ambiente de Desenvolvimento

---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, sans-serif;
+  }
+`
+
+export default EstiloGlobal
